918b7b118fb21e786ad36a0ff874625548c7073b
codingotaku <codingotaku@users.noreply.github.com>
2019-09-07 21:00:46 +0530

    Added builder and removed hard coded sources

--- src/main/java/com/codingotaku/apis/animecrawler/Source.java
+++ src/main/java/com/codingotaku/apis/animecrawler/Source.java
@@ -1,33 +1,98 @@
 package com.codingotaku.apis.animecrawler;
 
 public class Source {
-	public static final Source Anime1;
-	public static final Source Sub;
-	public static final Source Dub;
-	static {
-		Anime1 = new Source()
-				.setListUrl("http://www.anime1.com/content/list/")
-				.setListRegex("div.alph-list-box > h4:has(a[name]) + ul > li > a")
-				.setNameRegex("h1.blue-main-title")
-				.setDocRegex("div.detail-left > span > span:eq(3)")
-				.setPosterRegex("div.detail-cover >a >img")
-				.setEpRegex("div.left-left > ul.anime-list > li > a");
+	public static class SourceBuilder {
+		private String docRegex;
+		private String epRegex;
+		private String epRegexAlt;
+		private String listRegex;
+		private String listUrl;
+		private String posterRegex;
+		private String vidRegex;
+		private String nameRegex;
+		private boolean multiPage;
+		private boolean multiEpList;
+		private PageNav pagePattern;
 
-		Sub = new Source()
-				.setListUrl("https://www.thewatchcartoononline.tv/subbed-anime-list")
-				.setListRegex("div.ddmcc ul > li > a")
-				.setNameRegex("div.h1-tag > a")
-				.setDocRegex("div#sidebar_cat > p")
-				.setPosterRegex("div#sidebar_cat > img")
-				.setEpRegex("div.cat-eps > a");
-		
-		Dub = new Source()
-				.setListUrl("https://www.thewatchcartoononline.tv/dubbed-anime-list")
-				.setListRegex("div.ddmcc ul > li > a")
-				.setNameRegex("div.h1-tag > a")
-				.setDocRegex("div#sidebar_cat > p")
-				.setPosterRegex("div#sidebar_cat > img")
-				.setEpRegex("div.cat-eps > a");
+		public SourceBuilder() {
+			vidRegex = "(http[s]?:\\/\\/[^\\/]*.*.mp4\\\\??[^\\\"\\']*)";
+			epRegexAlt = null;
+			multiPage = false;
+			multiEpList = false;
+			pagePattern = null;
+		}
+
+		public SourceBuilder setDocRegex(String docRegex) {
+			this.docRegex = docRegex;
+			return this;
+		}
+
+		public SourceBuilder setEpRegex(String epRegex) {
+			this.epRegex = epRegex;
+			return this;
+		}
+
+		public SourceBuilder setEpRegexAlt(String string) {
+			this.epRegexAlt = string;
+			return this;
+		}
+
+		public SourceBuilder setListRegex(String listRegex) {
+			this.listRegex = listRegex;
+			return this;
+		}
+
+		public SourceBuilder setListUrl(String listUrl) {
+			this.listUrl = listUrl;
+			return this;
+		}
+
+		public SourceBuilder setPosterRegex(String posterRegex) {
+			this.posterRegex = posterRegex;
+			return this;
+		}
+
+		public SourceBuilder setVidRegex(String vidRegex) {
+			this.vidRegex = vidRegex;
+			return this;
+		}
+
+		public SourceBuilder setMultiPage(boolean multiPage) {
+			this.multiPage = multiPage;
+			return this;
+		}
+
+		public SourceBuilder setMultiEpList(boolean multiEpList) {
+			this.multiEpList = multiEpList;
+			return this;
+		}
+
+		public SourceBuilder setNameRegex(String nameRegex) {
+			this.nameRegex = nameRegex;
+			return this;
+		}
+
+		public SourceBuilder setPageNav(PageNav pattenrs) {
+			this.pagePattern = pattenrs;
+			return this;
+		}
+
+		public Source build() {
+			Source source = new Source();
+			source.docRegex		= this.docRegex;
+			source.epRegex		= this.epRegex;
+			source.epRegexAlt	= this.epRegexAlt;
+			source.listRegex	= this.listRegex;
+			source.listUrl		= this.listUrl;
+			source.posterRegex	= this.posterRegex;
+			source.vidRegex		= this.vidRegex;
+			source.nameRegex	= this.nameRegex;
+			source.multiPage	= this.multiPage;
+			source.multiEpList	= this.multiEpList;
+			source.pagePattern	= this.pagePattern;
+			return source;
+		}
+
 	}
 
 	private String docRegex;
@@ -42,7 +107,7 @@ public class Source {
 	private boolean multiEpList;
 	private PageNav pagePattern;
 
-	private Source() {
+	public Source() {
 		vidRegex = "(http[s]?:\\/\\/[^\\/]*.*.mp4\\\\??[^\\\"\\']*)";
 		epRegexAlt = null;
 		multiPage = false;
@@ -50,101 +115,47 @@ public class Source {
 		pagePattern = null;
 	}
 
-	PageNav pageNav() {
+	public PageNav pageNav() {
 		return pagePattern;
 	}
 
-	String docRegex() {
+	public String docRegex() {
 		return docRegex;
 	}
 
-	String epRegex() {
+	public String epRegex() {
 		return epRegex;
 	}
 
-	String epRegexAlt() {
+	public String epRegexAlt() {
 		return epRegexAlt;
 	}
 
-	String listRegex() {
+	public String listRegex() {
 		return listRegex;
 	}
 
-	String listUrl() {
+	public String listUrl() {
 		return listUrl;
 	}
 
-	String posterRegex() {
+	public String posterRegex() {
 		return posterRegex;
 	}
 
-	String vidRegex() {
+	public String vidRegex() {
 		return vidRegex;
 	}
-	
-	String nameRegex() {
+
+	public String nameRegex() {
 		return this.nameRegex;
 	}
-	boolean isMultiPage() {
+
+	public boolean isMultiPage() {
 		return multiPage;
 	}
 
-	boolean isMultiEpList() {
+	public boolean isMultiEpList() {
 		return multiEpList;
-	}
-
-	private Source setDocRegex(String docRegex) {
-		this.docRegex = docRegex;
-		return this;
-	}
-
-	private Source setEpRegex(String epRegex) {
-		this.epRegex = epRegex;
-		return this;
-	}
-
-	private Source setEpRegexAlt(String string) {
-		this.epRegexAlt = string;
-		return this;
-	}
-
-	private Source setListRegex(String listRegex) {
-		this.listRegex = listRegex;
-		return this;
-	}
-
-	private Source setListUrl(String listUrl) {
-		this.listUrl = listUrl;
-		return this;
-	}
-
-	private Source setPosterRegex(String posterRegex) {
-		this.posterRegex = posterRegex;
-		return this;
-	}
-
-	private Source setVidRegex(String vidRegex) {
-		this.vidRegex = vidRegex;
-		return this;
-	}
-
-	private Source setMultiPage(boolean multiPage) {
-		this.multiPage = multiPage;
-		return this;
-	}
-
-	private Source setMultiEpList(boolean multiEpList) {
-		this.multiEpList = multiEpList;
-		return this;
-	}
-
-	private Source setNameRegex(String nameRegex) {
-		this.nameRegex = nameRegex;
-		return this;
-	}
-	
-	private Source setPageNav(PageNav pattenrs) {
-		this.pagePattern = pattenrs;
-		return this;
 	}
 }
