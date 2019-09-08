f4003e84de4c97f1659aac1fb605993694b0f9d0
codingotaku <codingotaku@users.noreply.github.com>
2019-09-08 09:03:27 +0530

    added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module anime_crawler_api {
+	exports com.codingotaku.apis.animecrawler.exception;
+	exports com.codingotaku.apis.animecrawler;
+	exports com.codingotaku.apis.animecrawler.helpers;
+	exports com.codingotaku.apis.animecrawler.callbacks;
+
+	requires com.google.common;
+	requires org.jsoup;
+}
